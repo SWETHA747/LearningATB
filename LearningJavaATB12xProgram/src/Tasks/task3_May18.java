b4122cd7880b2ea57be3e1a9d33738c7fa03b4ec
skusupud <skusupud@LIN-5CG5080LD7.corp.capgemini.com>
2025-05-20 18:35:16 +0530

    Commit

--- LearningJavaATB12xProgram/src/Tasks/task3_May18.java
+++ LearningJavaATB12xProgram/src/Tasks/task3_May18.java
@@ -6,7 +6,7 @@ public class task3_May18 {
 
 		int i = 1, j = 2, k = 3;
 
-		int m = i-- - j-- - k--;  // i-- = 0, j-- = 1, k-- =2   // 0 - 1 - 2  1-2-3=1-5=-4
+		int m = i-- - j-- - k--;  // i-- = 0, j-- = 1, k-- =2   
 
 		System.out.println("i = " + i + ", j = " + j + ", k = " + k + ", m = " + m);
 	}
