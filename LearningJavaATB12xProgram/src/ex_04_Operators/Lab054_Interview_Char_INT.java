dcc9b36dee094fd4bfc4f165b65fb00b7b1b864f
skusupud <skusupud@LIN-5CG5080LD7.corp.capgemini.com>
2025-05-23 19:55:38 +0530

    Initial Commit

---
+++ LearningJavaATB12xProgram/src/ex_04_Operators/Lab054_Interview_Char_INT.java
@@ -0,0 +1,17 @@
+package ex_04_Operators;
+
+public class Lab054_Interview_Char_INT {
+
+	public static void main(String[] args) {
+
+		char a = 'A'; // ASCII -> 65
+        char b = 'B'; // 66
+        char c = 'C'; //67
+        System.out.println(a + b);
+        // a+b - 65+66 -> 131
+        
+        System.out.println(a + c);
+        //a+c - 65+67  -> 132
+	}
+
+}
