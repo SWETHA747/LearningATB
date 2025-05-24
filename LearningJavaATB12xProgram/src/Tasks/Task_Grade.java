a33b52a29038d77b1b5c46712f7b811902b8da57
skusupud <skusupud@LIN-5CG5080LD7.corp.capgemini.com>
2025-05-24 20:28:00 +0530

    Initial Commit

---
+++ LearningJavaATB12xProgram/src/Tasks/Task_Grade.java
@@ -0,0 +1,42 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task_Grade {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Grade:");
+		String grade = sc.next();
+		grade=grade.toUpperCase();
+		
+		switch (grade) {
+		
+		case "A":
+			System.out.println("Excellent");
+			break;
+			
+		case "B":
+			System.out.println("Very Good");
+			break;
+
+		case "C":
+			System.out.println("Good");
+			break;
+
+		case "D":
+			System.out.println("Needs Improvement");
+			break;
+
+		case "F":
+			System.out.println("Fail");
+			break;
+
+		default:
+			System.out.println("Invalid grade");
+		}
+
+	}
+
+}
