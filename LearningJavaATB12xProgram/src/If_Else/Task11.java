1bff6aaf1318b9665b025977026d877aaaa9e818
skusupud <skusupud@LIN-5CG5080LD7.corp.capgemini.com>
2025-06-20 21:45:28 +0530

    Initial Commit

---
+++ LearningJavaATB12xProgram/src/If_Else/Task11.java
@@ -0,0 +1,25 @@
+package If_Else;
+import java.util.Scanner;
+
+public class Task11 {
+
+	public static void main(String[] args) {
+		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter the character");
+//		char ch = sc.next().charAt(0);
+		
+		char ch = 's';
+		
+		if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
+		{
+			System.out.println("It is an alphabet");
+		}
+		else
+		{
+			System.out.println("It is not an alphabet");
+		}
+		
+	}
+
+}
