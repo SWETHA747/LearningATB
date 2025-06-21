419fd9e760b6c7aa696d987706b264bb2cf842a5
skusupud <skusupud@LIN-5CG5080LD7.corp.capgemini.com>
2025-06-21 11:49:50 +0530

    Initial Commit

---
+++ LearningJavaATB12xProgram/src/LiveTest/Coding1.java
@@ -0,0 +1,21 @@
+package LiveTest;
+
+public class Coding1 {
+
+	public static void main(String[] args) 
+	{
+		
+		int i = 25;
+		double d = 3.14;
+		boolean b = true;
+		char ch = 'A';
+		
+		
+		System.out.println("Integer : " + i);
+		System.out.println("Double : " + d);
+		System.out.println("Boolean : " + b);
+		System.out.println("Character : " + ch);
+
+	}
+
+}
