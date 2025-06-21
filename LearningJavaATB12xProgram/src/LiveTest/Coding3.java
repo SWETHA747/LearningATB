bba766ada067a48ebab7403a5d7fcdf53ca2bc80
skusupud <skusupud@LIN-5CG5080LD7.corp.capgemini.com>
2025-06-21 14:59:02 +0530

    Initial Commit

---
+++ LearningJavaATB12xProgram/src/LiveTest/Coding3.java
@@ -0,0 +1,32 @@
+package LiveTest;
+
+public class Coding3 {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 8;
+		
+		System.out.println("Comparison Operators");
+		//Use comparison operators (==, !=, <, >, <=, >=)
+		System.out.println("a==b : " + (a==b));
+		System.out.println("a!=b : " + (a!=b));
+		System.out.println("a<b : "  + (a<b));
+		System.out.println("a>b : " + (a>b));
+		System.out.println("a<=b : " + (a<=b));
+		System.out.println("a>=b : " + (a>=b));
+		
+		
+		//Use logical operators (&&, ||, !)
+		System.out.println("\nLogical Operators");
+		boolean c = true;
+		boolean d = false;
+		
+		System.out.println("c&&d : " + (c&&d));
+		System.out.println("c||d : " + (c||d));
+		System.out.println("!c : " + (!c));
+		System.out.println("!d : " + (!d));
+
+	}
+
+}
