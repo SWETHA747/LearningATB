5c71dae401ca0598d0568265cb3a0f70495ca286
skusupud <skusupud@LIN-5CG5080LD7.corp.capgemini.com>
2025-06-21 12:34:51 +0530

    Initial Commit

---
+++ LearningJavaATB12xProgram/src/LiveTest/Coding4.java
@@ -0,0 +1,23 @@
+package LiveTest;
+
+public class Coding4 {
+
+	public static void main(String[] args) {
+		
+		int i = 5;
+		
+		System.out.println("Original Number : " + i);
+		
+		System.out.println("Pre-increment : " + ++i);
+		
+		System.out.println("Post-increment : " + i++);
+		
+		i =6;
+		
+		System.out.println("Pre-decrement : " + --i);
+		
+		System.out.println("Post-decrement : " + i--);
+
+	}
+
+}
