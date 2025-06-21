6060cf9c959d6afc2c907769e8bb0eb1e549dd37
skusupud <skusupud@LIN-5CG5080LD7.corp.capgemini.com>
2025-06-21 12:31:10 +0530

    Initial Commit

---
+++ LearningJavaATB12xProgram/src/LiveTest/Coding8.java
@@ -0,0 +1,23 @@
+package LiveTest;
+
+import java.util.Scanner;
+
+public class Coding8 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Year");
+		int year = sc.nextInt();
+		
+		if((year%4==0 && year%100!=0) || (year%400==0))
+		{
+			System.out.println("It is a leap year");
+		}
+		else
+		{
+			System.out.println("It is not a leap year");
+		}
+	}
+
+}
