3d2d87b140f48a428cb9b3c2a11ea196ab28a663
skusupud <skusupud@LIN-5CG5080LD7.corp.capgemini.com>
2025-06-22 12:42:57 +0530

    Commit

--- LearningJavaATB12xProgram/src/CodingTest/BankAccount.java
+++ LearningJavaATB12xProgram/src/CodingTest/BankAccount.java
@@ -1,9 +1,35 @@
 package CodingTest;
 
 public class BankAccount {
+	private double balance;
 
+	public void deposit(double amount)
+	{
+		if(amount > 0) {
+			balance = balance + amount;
+			System.out.println("Balance after deposit :" + balance);
+		}
+	}
+	
+	public void withdraw(double amount)
+	{
+		if(amount>0 && amount<=balance)
+		{
+			balance = balance - amount;
+			System.out.println("Balance after withdrawl :" + balance);
+		}
+		else
+		{
+			System.out.println("Invalid withdrawl");
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 BankAccount myaccount = new BankAccount();
+		 
+		 myaccount.deposit(50000);
+		 myaccount.withdraw(30000);
+		 //myaccount.withdraw(60000);
 
 	}
 
