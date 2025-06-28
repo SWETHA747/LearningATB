c7a68a7ea037a04adb1ca83e34cc561e50fa03bf
skusupud <skusupud@LIN-5CG5080LD7.corp.capgemini.com>
2025-06-28 10:51:11 +0530

    Initial Commit

---
+++ LearningJavaATB12xProgram/src/StringsTest/Coding1.java
@@ -0,0 +1,19 @@
+package StringsTest;
+
+public class Coding1 {
+
+	public static void main(String[] args) {
+		
+		String s = "Hello World";
+		
+		System.out.println("Concatenation : " +s.concat("!!"));
+		
+		System.out.println("Length is : " + s.length());
+		
+		System.out.println("Substring is : " + s.substring(6));
+		
+		System.out.println("First Character is : " + s.charAt(0));
+
+	}
+
+}
